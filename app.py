78c50fb17b13ca8ea6166d0aafb72309822b8ce2
minhaj-raheem <minhajraheem@live.com>
2017-10-04 15:16:51 +0530

    wsgi

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def qsubmit():
 
 
 # Main Method
-if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='127.0.0.1', port=5000)
+#if __name__ == '__main__':
+app.secret_key = 'super_secret_key'
+    #app.debug = True
+    #app.run(host='127.0.0.1', port=5000)
